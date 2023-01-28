e30d56847d3508eff27fd24c423811aa28fdc7bc
Muhammad Saqib <msaqib@gordiansol.com>
2023-01-28 17:18:43 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+Promise = require("bluebird");
+const { port } = require("./config/vars");
+// const mongoose = require('./config/mongoose');
+var models = require("../src/api/models");
+const app = require("./config/express");
+app.set("view engine", "ejs");
+const http = require("http");
+// mongoose.connect();
+models.sequelize.sync().then(function () {
+  //   app.listen(port);
+  http.createServer(app).listen(port);
+});
+// http.createServer(app).listen(port);
+// console.log(`App is running on ${port}`);
+module.exports = app;
